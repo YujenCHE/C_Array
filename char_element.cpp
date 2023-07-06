2e98731d8d895a34158936b2adb7992819950f5e
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-07-06 08:45:25 +0800

    Update char_element.cpp

--- char_element.cpp
+++ char_element.cpp
@@ -3,7 +3,7 @@
 int main() {
 	
 	char arr[5] = {'S', 'H', 'A', 'R', 'E'};
-	printf("The 2nd element is %c.\n", arr[1]);
+	printf("The 2nd element is %c.\n", arr[1]);//從0開始算
 	printf("The 3rd element is %c.\n", arr[2]);
 	return 0;
 }
