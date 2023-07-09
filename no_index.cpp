295a7ec6a6491576d33f53213b554d0ae9b45986
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-07-09 16:48:36 +0800

    Create no_index.cpp

---
+++ no_index.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int arr[6];
+	
+	//printf("%d\n", arr[1490]); too big
+	printf("%d\n", arr[2]);
+	printf("%d\n", arr[-12]);
+	printf("%d\n", arr[6]);
+	
+	//all get unexpected output
+	
+	return 0;
+}
